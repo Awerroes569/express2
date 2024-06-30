156634bde6346f8d5ab6f3655a6bae047b35cae6
AS <arssoo569@gmail.com>
2024-06-30 16:46:37 +0200

    adding routes and middlewares

--- server.js
+++ server.js
@@ -1,6 +1,52 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+//MIDDLEWARES
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use((req, res, next) => {
+    res.show = (name) => {
+      res.sendFile(path.join(__dirname, `/views/${name}`));
+    };
+    next();
+  });
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+//ENDPOINTS
+
+app.get('/', (req, res) => {
+    res.show('home.html');
+});
+
+app.get('/home', (req, res) => {
+res.show('home.html');
+});
+
+app.get('/about', (req, res) => {
+    res.show('about.html');
+  });
+
+app.get('/user/*', (req, res) => {
+    res.show('forbidden.html');
+  });
+
+//ERROR HANDLING
+
+app.use((req, res) => {
+res.status(404).send('404 not found...');
+})
+
+
+//LISTENING
 
 
 app.listen(8000, () => {
