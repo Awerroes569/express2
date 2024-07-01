d0ddf0f9b888f909c18f93fc4c13555ede91af25
AS <arssoo569@gmail.com>
2024-07-01 14:56:03 +0200

    installing handlebars

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ app.get('/home', (req, res) => {
 res.show('home.html');
 });
 
+app.get('/hello/:name', (req, res) => {
+res.send(`Hello, ${req.params.name}`);
+});
+
+
+app.get('/about', (req, res) => {
+    res.show('about.html');
+  });
+  
 app.get('/about', (req, res) => {
     res.show('about.html');
   });
