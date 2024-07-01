33a073510ef0bded0d9a0a549f59ba51dd69e642
AS <arssoo569@gmail.com>
2024-07-01 20:08:06 +0200

    cleaning code

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const hbs = require('express-handlebars');
 const app = express();
 
 //HANDLEBARS
+
 app.engine('.hbs', hbs.engine());
 app.set('view engine', '.hbs');
 
@@ -15,36 +16,34 @@ app.use(express.static(path.join(__dirname, '/public')));
 //ENDPOINTS
 
 app.get('/', (req, res) => {
-  res.render('home', {layout: false});
+  res.render('home');
 });
 
 app.get('/home', (req, res) => {
-  res.render('home', {layout: false});
+  res.render('home');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
-
 app.get('/about', (req, res) => {
-  res.render('about', {layout: false});
+  res.render('about');
 });
 
 app.get('/user/*', (req, res) => {
-  res.render('forbidden', {layout: false});
+  res.render('forbidden');
 });
 
 //ERROR HANDLING
 
 app.use((req, res) => {
-  res.render('404', {layout: false});
+  res.render('404');
 })
-
 
 //LISTENING
 
 
 app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
-  });
+  console.log('Server is running on port: 8000');
+});
